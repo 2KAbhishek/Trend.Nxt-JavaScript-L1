109280213a866d7ab19998be8af9167e5c746343
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-02-25 02:28:11 +0530

    Add primeNums.js

---
+++ Topic-4/Assignment-1/primeNums.js
@@ -0,0 +1,16 @@
+let inp = prompt("Enter the limit");
+
+let res = [1];
+let k = 1;
+for (let curNum = 2; curNum <= inp; curNum++) {
+  let isPrime = true;
+  for (let divNum = 2; divNum <= Math.sqrt(curNum); divNum++) {
+    if (curNum % divNum == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) res[k++] = curNum;
+}
+console.log(res);
+document.write(res);
