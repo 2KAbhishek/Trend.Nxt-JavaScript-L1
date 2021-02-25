a47fe8381820bae13d3c6ea88d575b0951cc0f09
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-02-25 22:36:54 +0530

    Update comments

--- Topic-4/Assignment-1/primeNums.js
+++ Topic-4/Assignment-1/primeNums.js
@@ -1,7 +1,10 @@
+// Get the input from user
 let inp = prompt("Enter the limit");
 
 let res = [1];
 let k = 1;
+
+/* Check if number till range is valid */
 for (let curNum = 2; curNum <= inp; curNum++) {
   let isPrime = true;
   for (let divNum = 2; divNum <= Math.sqrt(curNum); divNum++) {
