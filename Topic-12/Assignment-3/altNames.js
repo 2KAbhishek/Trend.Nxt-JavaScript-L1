4408a06a3da1f6a336197b2e4cd16aace8844126
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-03-04 01:08:39 +0530

    Add altNames.js

---
+++ Topic-12/Assignment-3/altNames.js
@@ -0,0 +1,7 @@
+var empNames = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+var n = empNames.length;
+for (var i = 0; i < n; i = i + 2) {
+  document.write(empNames[i] + "<br>");
+  console.log(empNames[i]);
+}
