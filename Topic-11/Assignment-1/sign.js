573766e045480d38dbdc2bba5ee03d23f5f1ad46
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-03-04 00:50:01 +0530

    Add sign.js

---
+++ Topic-11/Assignment-1/sign.js
@@ -0,0 +1,7 @@
+let a = parseInt(prompt("Enter Num 1: "));
+let b = parseInt(prompt("Enter Num 2: "));
+let c = parseInt(prompt("Enter Num 3: "));
+
+let res = a * b * c;
+
+res >= 0 ? alert("Sign is +") : alert("Sign is -");
