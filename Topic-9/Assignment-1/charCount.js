48f65379757b987bbe840e5dcd9fc1f61b6579b7
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-03-03 22:27:24 +0530

    Add charCount.js

--- Topic-9/Assignment-1/charCount.js
+++ Topic-9/Assignment-1/charCount.js
@@ -1,7 +1,7 @@
 let str = prompt("Enter a string: ");
-let ch = prompt("Enter character to search: ");
 
-function charCount(str, ch) {
+function charCount(str) {
+  let ch = prompt("Enter character to search: ");
   c = 0;
   for (let pos = 0; pos < str.length; pos++) {
     if (ch == str.charAt(pos)) {
@@ -11,5 +11,7 @@ function charCount(str, ch) {
   return c;
 }
 
-document.write("Count of " + ch + " in " + str + " is " + charCount(str, ch));
-console.log("Count of " + ch + " in " + str + " is " + charCount(str, ch));
+let count = charCount(str);
+
+document.write("Count is: " + count);
+console.log("Count is: " + count);
